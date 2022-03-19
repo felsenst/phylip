266add2ac539cf0490334466752ed6d541d2cc87
Joe Felsenstein <felsenst@gmail.com>
2022-03-18 18:10:16 -0700

    reform of ML intermediate classes

--- seq.c
+++ seq.c
@@ -19,6 +19,30 @@ sequence inputSequences;
 node** lrsaves;
 
 
+void allocx(long nonodes, long endsite, long param, ml_node** treenode)
+{
+  /* allocate sequences */
+  /* param =  sitelength in restml */
+  /* param =  rcategs in dnaml/proml */
+  long i;
+  ml_node *p;
+  ml_node *q;
+
+  for (i = 0; i < spp; i++)
+    treenode[i]->allocx((node*)treenode[i], endsite, param);
+  for (i = spp; i < nonodes; i++)
+  {
+    p = treenode[i];
+    q = p;
+    do
+    {
+      q->allocx((node*)q, endsite, param);
+      q = (ml_node*)q->node.next;
+    } while ( q != p);
+  }
+}  /* allocx */
+
+
 void inputdata(long chars)
 {
   /* read in and if needed print out sequences */
