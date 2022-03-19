a099378676f2a02dcee859f827e54d814976cc7c
Joe Felsenstein <felsenst@gmail.com>
2022-03-18 18:08:34 -0700

    major reform of intermediate classes for ML

--- mlprot.c
+++ mlprot.c
@@ -65,3 +65,63 @@ void prot_node_init(node *n, node_type type, long index)
 } /* prot_node_init */
 
 
+void prot_node_freex(ml_node* n)
+{
+  /* free a protein tree node */
+  prot_node *pn;
+  long i;
+
+  pn = (prot_node *)n;
+  for ( i = 0 ; i < n->endsite ; i++ )
+  {
+    free(pn->x[i]);
+  }
+
+  free(pn->x);
+  pn->x = NULL;
+  free(n->underflows);
+  n->underflows = NULL;
+} /* prot_node_freex */
+
+
+void prot_node_allocx(ml_node* nn, long endsite, long rcategs)
+{
+  /* allocate space for sequences on a protein tree node */
+  prot_node *n = (prot_node *)nn;
+  long i;
+
+  n->ml_node.categs = rcategs;
+  n->ml_node.endsite = endsite;
+
+  n->x = (pphenotype)Malloc(endsite * sizeof(pratelike));
+  for ( i = 0 ; i < endsite ; i++ )
+    n->x[i] = (pratelike)Malloc(rcategs * sizeof(psitelike));
+  n->ml_node.underflows= Malloc(endsite * sizeof(double));
+} /* prot_node_allocx */
+
+
+void prot_freex_notip(long nonodes, pointarray treenode)
+{
+  /* free interior fork nodes
+   * used in proml */
+  long i, j;
+  node *p;
+
+  for (i = spp; i < nonodes; i++)
+  {
+    p = treenode[i];
+    if ( p == NULL ) continue;
+    do {
+      for (j = 0; j < endsite; j++)
+      {
+        free(((prot_node*)p)->x[j]);
+        ((prot_node*)p)->x[j] = NULL;
+      }
+      free(((prot_node*)p)->x);
+      ((prot_node*)p)->x = NULL;
+      p = p->next;
+    } while (p != treenode[i]);
+  }
+}  /* prot_freex_notip */
+
+
