2a2837c65b373ece60e6a53b5c033a4921877137
Joe Felsenstein <joe@gs.washington.edu>
2022-06-12 18:12:24 -0700

    test only, will revert later

--- dnaml.c
+++ dnaml.c
@@ -1,6 +1,7 @@
 /* Version 4.0.
    Written by Joseph Felsenstein, Akiko Fuseki, Sean Lamont, Andrew Keeffe,
    Dan Fineman, Patrick Colacurcio, Elizabeth Walkup, and Jim McGill. */
+/* test 6/12/22 */
 
 
 #ifdef HAVE_CONFIG_H
