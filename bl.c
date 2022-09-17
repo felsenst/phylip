96e893f56f599738965ff06f618beeaea9326f57
felsenst <felsenst@gmail.com>
2022-09-16 17:32:48 -0700

    putting bl.c, bl.h between  ml.c, ml.h and generic functions in phylip.c, phylip.h

--- bl.c
+++ bl.c
@@ -33,47 +33,38 @@ extern boolean usertree, lngths, smoothit, smoothed, polishing;
 boolean inserting;
 
 
-void ml_tree_new(struct tree **tp, long nonodes, long spp, long treesize)
+void bl_tree_new(struct tree **tp, long nonodes, long spp, long treesize)
 { /* make a new ml_tree.  Calls to generic_tree_new,
    * casting ml_tree** to tree** as we call it 
    * then call  ml_tree_init */
 
-  bl_tree_new(tp, nonodes, spp, treesize);        /* next up tree hierarchy */
-} /* ml_tree_new */
+  generic_tree_new(tp, nonodes, spp, treesize);   /* next up tree hierarchy */
+} /* bl_tree_new */
 
 
-void ml_tree_init(struct tree* t, long nonodes, long spp)
-{ /* set up function variables in ml_tree.  Currently these are actually
+void bl_tree_init(struct tree* t, long nonodes, long spp)
+{ /* 
    * attributes of the generic tree that need ml function versions */
 
-  bl_tree_init(t, nonodes, spp);                   /* go up class hierarchy */
-  t->smoothall = ml_tree_smoothall;
-  t->insert_ = (tree_insert_t)ml_tree_insert_;
-  t->re_move = ml_tree_re_move;
-  t->try_insert_ = (tree_try_insert_t)ml_tree_try_insert_;
-  t->do_branchl_on_insert_f = ml_tree_do_branchl_on_insert;
-  t->do_branchl_on_re_move_f = ml_tree_do_branchl_on_re_move;
-/* debug: need here?   ((ml_tree*)t)->nuview = ml_tree_nuview;
-  (t.tree)->makenewv_t = ml_tree->makenewv_t;
- * */
-} /* ml_tree_init */
+  generic_tree_init(t, nonodes, spp);              /* go up class hierarchy */
+} /* bl_tree_init */
 
 
-struct node* ml_node_new(node_type type, long index, long nodesize) {
+struct node* bl_node_new(node_type type, long index, long nodesize) {
   /* go up hierarchy creating a node, initializing it */
   struct node* nn;
 
   nn = generic_node_new(type, index, nodesize);
-  ml_node_init(nn, type, index);
+  bl_node_init(nn, type, index);
   return nn;
-} /* ml_node_new */
+} /* bl_node_new */
 
 
-void ml_node_init(struct node *n, node_type type, long index)
+void bl_node_init(struct node *n, node_type type, long index)
 {
   /* initialize a node for ml trees */
 /* debug: not needed for dist_node creation but needed for sequence types.  Needs nodesize argument? probably not */
-  ml_node* nn;
+  bl_node* nn;
 
   // RSGdebug: "index" should be > 0 if used for array access.  Can be 0 only
   // for initialization where it will be changed to > 0 before used for access.
@@ -81,12 +72,7 @@ void ml_node_init(struct node *n, node_type type, long index)
   assert(index >= 0);
 
   generic_node_init(n, type, index);                /* go up node hierarchy */
-  n->copy = ml_node_copy;
-  n->free = ml_node_free;
-  n->reinit = ml_node_reinit;
-  n->node_print_f = ml_node_print;
-  nn = (ml_node*)n;
-  nn->freex = NULL;         /* x is only defined for dna_node and prot_node */
+  nn = (bl_node*)n;
   nn->node.tyme = 0;
 } /* ml_node_init */
 
---
+++ bl.h
@@ -0,0 +1,93 @@
+/* Version 4.0. (c) Copyright 1993-2013 by the University of Washington.
+   Written by Michal Palczewski
+   Permission is granted to copy and use this program provided no fee is
+   charged for it and provided that this copyright notice is not removed. */
+
+/* debug:  would it be better to split ml_tree / ml_node into two levels of
+ * the hierarchy, so ml_node / ml_tree is for cases where the branch lengths
+ * are iterated, and the codon_node / prot_node / dna_node are for cases
+ * where it not only does that, it has molecular sequences
+ * maybe call these  iterate.c / iterate.h  and  sequence.c / sequence.h ? */ 
+
+#ifndef _ML_H_
+#define _ML_H_
+
+#include "phylip.h"
+
+extern boolean inserting, smoothit, smoothed, polishing;
+
+struct allocx_t;
+struct freex_t;
+
+typedef void (*allocx_t)(struct node*, long, long);
+typedef void (*freex_t)(node*);
+
+typedef struct ml_tree {
+  struct tree treepart;
+} ml_tree;
+
+typedef struct ml_node {                       /* subclass of generic node */
+  struct node node;                          /* Base object, must be first */
+  allocx_t allocx;
+  freex_t freex;    /* debug: stuff after here to be later moved to mldna.c ? */
+  double* underflows;
+  long endsite;
+  long categs;
+} ml_node;
+
+typedef void (*makenewv_t)(tree*, node*);
+typedef void (*nuview_t)(tree*, node*);
+
+typedef void (*initialvtrav_t)(tree*, node*);
+
+
+long endsite;
+
+#ifndef OLDC /* prototypes */
+void    ml_tree_new(struct tree**, long, long, long);
+void    ml_tree_init(struct tree*, long, long);
+node*   ml_node_new(node_type, long, long);
+void    ml_node_init(struct node*, node_type, long);
+void    ml_node_free(node **);
+void    ml_node_print(node *);
+void    ml_node_copy(node*, node*);
+void    ml_hookup(node*, node*);
+void    allocx(long, long, long, ml_node**);
+void    makevalues2(long, pointarray, long, long, sequence, steptr);
+void    freex_notip(long, pointarray);
+void    freex(long, pointarray);
+void    ml_update(tree*, node *);
+void    smooth(tree*, node *);
+void    smooth_traverse(tree*, node *);
+void 	ml_tree_smoothall(tree*, node*);
+void 	ml_node_reinit(node * n);
+void    ml_tree_insert_(tree*, node*, node*, boolean);
+void    ml_tree_re_move(tree*, node*, node**, boolean);
+boolean ml_tree_try_insert_(tree* , node* , node* , node* , double*, tree*,
+                            boolean, boolean, boolean, double*);
+boolean ml_tree_try_insert_thorough(tree*, node*, node*, node*, 
+                          double*, tree*, boolean, boolean, boolean);
+void    ml_tree_do_branchl_on_insert(tree*, node *, node*);
+void    ml_tree_do_branchl_on_re_move(tree*, node*, node*);
+void    mlk_tree_insert_(tree*, node *, node *, boolean, boolean);
+double  get_tyme(node *);
+void    set_tyme (node*, double) ;
+void    mlk_tree_re_move(tree* t, node *item, node** where, boolean recompute);
+void    getthree(tree*, node *, double, double, double, double *, double *);
+double  min_child_tyme(node *);
+double  parent_tyme(node *);
+boolean valid_tyme(tree *, node *, double);
+double  set_tyme_evaluate(tree *, node *, double);
+void    mlk_tree_makenewv(tree*, node *);
+void    empiricalfreqs(double *, double *, double *, double *, steptr, pointarray);
+void    ml_treeoutrecurs(FILE*, tree*, node*, double, int*);
+void    ml_treeout(FILE*, tree*, node*, double);
+void    ml_treevaluate(tree*, boolean, boolean, boolean, boolean, tree*,
+                        tree*, initialvtrav_t);
+void    ml_initialvtrav(tree*, node *);
+#endif
+
+#endif /* _ML_H_ */
+
+
+/* End.*/
