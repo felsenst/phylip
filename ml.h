80710686528d00933bb2008b6e0d44d4a16c427e
Joe Felsenstein <felsenst@gmail.com>
2025-02-16 00:18:52 +0000

    trying to get declarations right

--- ml.h
+++ ml.h
@@ -9,12 +9,12 @@
  * where it not only does that, it has molecular sequences
  * maybe call these  iterate.c / iterate.h  and  sequence.c / sequence.h ? */ 
 
-#ifndef ML_H
-#define ML_H
-
-#ifdef BL_H
+#ifndef BL_H
 #include "bl.h"
 #endif
+
+#ifndef ML_H
+#define ML_H
 
 /* debug: extern boolean inserting, smoothit, polishing; */
 
