4c247ddddbd8d0dfe36141d0a64b2cc0a755ac23
Joe Felsenstein <felsenst@gmail.com>
2025-12-18 21:04:10 -0800

    r and such

--- seq.c
+++ seq.c
@@ -698,9 +698,12 @@ void drawline2(long i, double scale, struct node *p, struct tree* curtree)
         putc(' ', outfile);
       if (r->back != 0) {                     /* if branch is not empty ... */
         drawline2(i, scale, r->back, curtree);          /* ... start out it */
+	r = r->next;
       }
+      else
+        done = true;
     }
-    else {
+    if (!done) {
       if (r->next->back == 0) {
         done = true;
       } else {
